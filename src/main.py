df86ed38b8f3ffc5568225919a32c842c737dcbb
Separatehhh23 <separatehhh23@gmail.com>
2023-05-11 18:13:38 +0200

    Fixed file structure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+# Cargar ibrerias
+import PySimpleGUI as sg
+import time
+
 # Cargar src
-from src.functions.handlers import *
+from src import *
+
+time.sleep(1)
 
 # Imprimir info
 print(f"Root directory: {ROOT_DIR}")
-
-# Cargar ibrerias
-import PySimpleGUI as sg
 
 # Crear layout vacio
 layout = []
---
+++ src/src/functions/handlers/fileHandler.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', '..', '..'))
+JSON_DIR = os.path.join(ROOT_DIR, 'src', 'src', 'json')
+QA_DIR =  os.path.join(JSON_DIR, 'QnA.json')
